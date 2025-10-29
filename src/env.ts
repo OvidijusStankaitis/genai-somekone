e62d460c43ce3b5179e839d2b72ef3c4e862c540
Nicolas Pope <nicolas.pope@uef.fi>
2025-10-29 22:35:38 +0000

    Merged PR 965: Fix URL.parse bug
    Fix URL.parse bug

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,18 @@
 const url = import.meta.env.VITE_APP_APIURL || window.location.origin || 'http://localhost:9000';
-const apiUrl = URL.parse(url);
+
+function safeParse(urlString: string) {
+    if (URL.parse) {
+        return URL.parse(urlString);
+    } else {
+        try {
+            return new URL(urlString);
+        } catch {
+            return null;
+        }
+    }
+}
+
+const apiUrl = safeParse(url);
 
 const isSecure = apiUrl?.protocol === 'https:';
 const host = apiUrl?.hostname || 'localhost';
